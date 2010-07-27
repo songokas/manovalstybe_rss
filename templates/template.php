f50cc5452d2559d7f5bcac4da52b6cf4f5b8162e
Emilis Dambauskas <emilis.d@gmail.com>
2010-07-27 10:55:01 +0300

    Fixed main report template.

--- templates/template.php
+++ templates/template.php
@@ -1,26 +1,18 @@
 <html>
     <body>
-<p>Tai yra automatiškai suformuota <a href="http://<?=$url?>"><?=$url?></a> darbų ir naujienų ataskaita.
-<br/>
-
+<p>Tai yra automatiškai suformuota <a href="http://<?=$url?>"><?=$url?></a> darbų ir naujienų ataskaita.</p>
 <p>Laikotarpis: <?=$config['from']?> &mdash; <?=$config['to']?></p>
 
-<br/>
-
+%s
 
 %s
 
+%s
 
 %s
 
-
-%s
-
-
-%s
-
-
-<p>Šią ataskaitą suformavo &lt;manovalstybe&gt; programėlė, kurios kodą galite rasti <a href="http://github.com/tomasj/manovalstybe_rss.git">čia</a>.</p>
+<hr>
+<p>Šią ataskaitą suformavo programėlė, kurios kodas prieinamas: <a href="http://github.com/tomasj/manovalstybe_rss.git">http://github.com/tomasj/manovalstybe_rss.git</a>.</p>
     </body>
 </html>
 
