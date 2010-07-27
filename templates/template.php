7ad39f1bcf3f9194bab7b316a36fa7b58241c076
Emilis Dambauskas <emilis.d@gmail.com>
2010-07-27 10:11:41 +0300

    Fixed messages in reports to comply with the spec.

--- templates/template.php
+++ templates/template.php
@@ -3,7 +3,7 @@
 <p>Tai yra automatiškai suformuota <a href="http://<?=$url?>"><?=$url?></a> darbų ir naujienų ataskaita.
 <br/>
 
-<p>Laikotarpis: &lt;<?=$config['from']?>&gt; -- &lt;<?=$config['to']?>&gt;</p>
+<p>Laikotarpis: <?=$config['from']?> &mdash; <?=$config['to']?></p>
 
 <br/>
 
@@ -20,7 +20,7 @@
 %s
 
 
-<p>Šią ataskaitą suformavo &lt;manovalstybe&gt; programėlė, kurios kodą galite rasti &lt;<a href="http://github.com/tomasj/manovalstybe_rss.git">čia</a>&gt;.</p>
+<p>Šią ataskaitą suformavo &lt;manovalstybe&gt; programėlė, kurios kodą galite rasti <a href="http://github.com/tomasj/manovalstybe_rss.git">čia</a>.</p>
     </body>
 </html>
 
