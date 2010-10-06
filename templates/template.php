2bab8c4cb827fe2b3855eb4a35c436656cf49d4f
Tomas Jakstas <bizabrazija@gmail.com>
2010-10-06 12:12:43 +0300

    changes to app structure, one template left, for all sites

--- templates/template.php
+++ templates/template.php
@@ -1,17 +1,16 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="lt" lang="lt">
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+</head>
     <body>
-<p>Tai yra automatiškai suformuota <a href="http://<?=$url?>"><?=$url?></a> darbų ir naujienų ataskaita.</p>
+<p>Tai yra automatiškai suformuota <a href="<?=$config['current_url']?>"><?=$config['current_name']?></a> darbų ir naujienų ataskaita.</p>
 <p>Laikotarpis: <?=$config['from']?> &mdash; <?=$config['to']?></p>
 
-%s
-
-%s
-
-%s
-
-%s
-
-%s
+<?= $git ?>
+<?= $site ?>
+<?= $teambox ?>
+<?= $stats ?>
 
 
 <hr/>
